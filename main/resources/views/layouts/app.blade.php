b173fad23029a07ccb6c3bfbbb2f558621a1a8da
Giancarlo Zuliani <slaigox@gmail.com>
2021-02-12 16:43:13 +0100

    header img added

--- main/resources/views/layouts/app.blade.php
+++ main/resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+                <img src="{{asset('storage/imgs/gervinho.jpg')}}" style="width:60px;height:80px" alt="">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
